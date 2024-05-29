cf4d1b4910fa0215360542a89b414ff6d2f051a4
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2024-05-29 17:52:53 -0500

    Version 2.0.4

--- core/src/main/java/site/ycsb/tpc/DataSource.java
+++ core/src/main/java/site/ycsb/tpc/DataSource.java
@@ -229,6 +229,16 @@ public class DataSource {
     return ((((randomUniformInt(0, A) | randomUniformInt(x, y)) + C) % (y - x + 1)) + x);
   }
 
+  static String lastName(int num) {
+    String name;
+
+    name = lastNameParts[num / 100];
+    name = name + lastNameParts[(num / 10) % 10];
+    name = name + lastNameParts[num % 10];
+
+    return name;
+  }
+
   public static double round(double value, int places) {
     BigDecimal bd = BigDecimal.valueOf(value);
     bd = bd.setScale(places, RoundingMode.HALF_UP);
