574367d5d00171fe2c36e5c9ece10d908ba5c437
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2024-07-08 18:04:39 -0500

    Version 2.0.4

--- core/src/main/java/site/ycsb/tpc/tpcc/SQLQueries.java
+++ core/src/main/java/site/ycsb/tpc/tpcc/SQLQueries.java
@@ -289,8 +289,9 @@ public class SQLQueries extends BenchQueries {
       "FROM (SELECT x.su_name " +
       "FROM (SELECT o1.o_id, o1.o_w_id, o1.o_d_id, ol1.ol_delivery_d,  " +
       "n.n_nationkey, su.su_suppkey, s.s_w_id, s.s_i_id, su.su_name " +
-      "FROM nation n, supplier su, stock s, orders o1, o1.o_orderline ol1 " +
-      "WHERE  o1.o_w_id = s.s_w_id " +
+      "FROM nation n, supplier su, stock s, orders o1, order_line ol1 " +
+      "WHERE o1.o_id = ol1.ol_o_id " +
+      "AND  o1.o_w_id = s.s_w_id " +
       "AND ol1.ol_i_id = s.s_i_id " +
       "AND s.s_w_id * s.s_i_id MOD 10000 = su.su_suppkey " +
       "AND ol1.ol_delivery_d > date_add_str(o1.o_entry_d, 150, 'day') " +
@@ -299,8 +300,9 @@ public class SQLQueries extends BenchQueries {
       "AND n.n_name = 'Peru') x " +
       "LEFT OUTER JOIN " +
       "(SELECT o2.o_id, o2.o_w_id, o2.o_d_id, ol2.ol_delivery_d " +
-      "FROM orders o2, o2.o_orderline ol2 " +
-      "WHERE o2.o_entry_d BETWEEN '2017-12-01 00:00:00' AND '2017-12-31 00:00:00') y " +
+      "FROM orders o2, order_line ol2 " +
+      "WHERE o2.o_id = ol2.ol_o_id " +
+      "AND o2.o_entry_d BETWEEN '2017-12-01 00:00:00' AND '2017-12-31 00:00:00') y " +
       "ON y.o_id = x.o_id AND y.o_w_id = x.o_w_id AND y.o_d_id = x.o_d_id " +
       "AND y.ol_delivery_d > x.ol_delivery_d " +
       "GROUP BY x.o_w_id, x.o_d_id, x.o_id, x.n_nationkey, x.su_suppkey, x.s_w_id, x.s_i_id, x.su_name " +
