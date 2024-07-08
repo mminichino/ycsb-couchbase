15e8591c8a08f6f83fac18cda8350d0b2ff98574
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2024-07-08 16:40:38 -0500

    Version 2.0.4

--- core/src/main/java/site/ycsb/tpc/tpcc/SQLQueries.java
+++ core/src/main/java/site/ycsb/tpc/tpcc/SQLQueries.java
@@ -177,8 +177,9 @@ public class SQLQueries extends BenchQueries {
       "THEN 1 ELSE 0 END) AS high_line_COUNT, " +
       "SUM (case when o.o_carrier_id <> 1 AND o.o_carrier_id <> 2 " +
       "THEN 1 ELSE 0 END) AS low_line_COUNT " +
-      "FROM orders o, o.o_orderline ol " +
-      "WHERE  o.o_entry_d <= ol.ol_delivery_d " +
+      "FROM orders o, order_line ol " +
+      "WHERE o.o_id = ol.ol_o_id " +
+      "AND  o.o_entry_d <= ol.ol_delivery_d " +
       "AND  ol.ol_delivery_d >= '2016-01-01 00:00:00.000000' AND  ol.ol_delivery_d < '2017-01-01 00:00:00.000000' " +
       "GROUP BY o.o_ol_cnt " +
       "ORDER BY o.o_ol_cnt",
