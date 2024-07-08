f5aeb49584c601ed22996ca1933356f36a53e8fe
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2024-07-08 16:51:43 -0500

    Version 2.0.4

--- core/src/main/java/site/ycsb/tpc/tpcc/SQLQueries.java
+++ core/src/main/java/site/ycsb/tpc/tpcc/SQLQueries.java
@@ -198,7 +198,7 @@ public class SQLQueries extends BenchQueries {
       "SELECT 100.00 * SUM(CASE WHEN i.i_data LIKE 'pr%' " +
       "THEN ol.ol_amount ELSE 0 END) / " +
       "(1+SUM(ol.ol_amount)) AS promo_revenue " +
-      "FROM item i, orders o, o.o_orderline ol " +
+      "FROM item i, order_line ol " +
       "WHERE ol.ol_i_id = i.i_id " +
       "AND ol.ol_delivery_d >= '2017-09-01 00:00:00.000000' AND ol.ol_delivery_d < '2017-10-01 00:00:00.000000'",
       // Q15
