29752cb65f9090a5781c0ddc36bfe4d6c9bf13b3
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2024-07-08 17:11:03 -0500

    Version 2.0.4

--- core/src/main/java/site/ycsb/tpc/tpcc/SQLQueries.java
+++ core/src/main/java/site/ycsb/tpc/tpcc/SQLQueries.java
@@ -229,12 +229,12 @@ public class SQLQueries extends BenchQueries {
       // Q17
       "SELECT SUM(ol.ol_amount) / 2.0 AS AVG_yearly " +
       "FROM  (SELECT i.i_id, AVG(ol1.ol_quantity) AS a " +
-      "FROM   item i, orders o1, o1.o_orderline ol1 " +
+      "FROM   item i, order_line ol1 " +
       "WHERE  i.i_data LIKE '%b' " +
       "AND  ol1.ol_i_id = i.i_id " +
-      "GROUP BY i.i_id) t, orders o, o.o_orderline ol " +
+      "GROUP BY i.i_id) t, order_line ol " +
       "WHERE ol.ol_i_id = t.i_id " +
-      "AND ol.ol_quantity < t.a",
+      "AND ol.ol_quantity <= t.a",
       // Q18
       "SELECT c.c_last, c.c_id o_id, o.o_entry_d, o.o_ol_cnt, SUM(ol.ol_amount) " +
       "FROM orders o, o.o_orderline ol, customer c " +
