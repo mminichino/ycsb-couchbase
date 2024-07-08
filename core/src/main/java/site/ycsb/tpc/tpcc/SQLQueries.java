52419a22d17f436d8c111b171c4b75f94c6cbe77
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2024-07-08 17:00:58 -0500

    Version 2.0.4

--- core/src/main/java/site/ycsb/tpc/tpcc/SQLQueries.java
+++ core/src/main/java/site/ycsb/tpc/tpcc/SQLQueries.java
@@ -204,7 +204,7 @@ public class SQLQueries extends BenchQueries {
       // Q15
       "WITH revenue AS ( " +
       "SELECT s.s_w_id * s.s_i_id MOD 10000 as supplier_no, SUM(ol.ol_amount) AS total_revenue " +
-      "FROM   stock s, orders o, o.o_orderline ol " +
+      "FROM   stock s, order_line ol " +
       "WHERE ol.ol_i_id = s.s_i_id " +
       "AND ol.ol_supply_w_id = s.s_w_id " +
       "AND ol.ol_delivery_d >= '2018-01-01 00:00:00.000000' AND ol.ol_delivery_d < '2018-04-01 00:00:00.000000' " +
