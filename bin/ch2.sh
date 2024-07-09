621aac886e135c2c0b2cb7dc994398ba5153e88c
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2024-07-09 09:07:50 -0500

    Version 2.0.4

---
+++ bin/ch2.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+#
+SCRIPT_PATH=$(dirname "$0")
+SCRIPT_ROOT=$(cd "$SCRIPT_PATH/.." && pwd)
+CLASSPATH="${SCRIPT_ROOT}/conf:${SCRIPT_ROOT}/lib/*"
+THREADCOUNT_LOAD=1
+THREADCOUNT_RUN=10
+RUNTIME=600
+RUN_MODE=0
+LOAD_MODE=0
+CBS_DRIVER_LOAD="site.ycsb.db.couchbase3.CouchbaseTPCLoad"
+COLUMNAR_DRIVER_LOAD="site.ycsb.db.couchbase3.AnalyticsTPCLoad"
+CBS_DRIVER_RUN="site.ycsb.db.couchbase3.CouchbaseTPCRun"
+COLUMNAR_DRIVER_RUN="site.ycsb.db.couchbase3.AnalyticsTPCRun"
+LOAD_DRIVER=$CBS_DRIVER_LOAD
+RUN_DRIVER=$CBS_DRIVER_RUN
+
+while getopts "T:lrSC" opt
+do
+  case $opt in
+    l)
+      LOAD_MODE=1
+      ;;
+    r)
+      RUN_MODE=1
+      ;;
+    T)
+      RUNTIME=$OPTARG
+      ;;
+    S)
+      LOAD_DRIVER=$CBS_DRIVER_LOAD
+      RUN_DRIVER=$CBS_DRIVER_RUN
+      ;;
+    C)
+      LOAD_DRIVER=$COLUMNAR_DRIVER_LOAD
+      RUN_DRIVER=$COLUMNAR_DRIVER_RUN
+      ;;
+    \?)
+      ;;
+    esac
+done
+
+if [ $LOAD_MODE -eq 1 ]; then
+  LOAD_OPTS="-db $LOAD_DRIVER -P workloads/workload_ch2 -threads $THREADCOUNT_LOAD -s -load"
+  java -cp "$CLASSPATH" site.ycsb.BenchClient $LOAD_OPTS
+fi
+
+if [ $RUN_MODE -eq 1 ]; then
+  RUN_OPTS="-db $RUN_DRIVER -P workloads/workload_ch2 -threads $THREADCOUNT_RUN -p operationcount=0 -p maxexecutiontime=$RUNTIME -manual -s -t"
+  java -cp "$CLASSPATH" site.ycsb.BenchClient $RUN_OPTS
+fi
